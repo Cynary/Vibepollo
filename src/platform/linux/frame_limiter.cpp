Nonary/vibeshine

fix(linux): apply stream color mode to external Proton launches
Games started from an existing desktop Steam process bypass the direct-launch environment. Keep a session-bound Proton hook active for SDR, 10-bit SDR, and HDR streams so external launches receive the current color mode, and repair inert hooks to remain valid Proton settings modules.

Generated with [Codex](https://openai.com/codex/)
Model: GPT 5.6-Sol High

src/platform/linux/frame_limiter.cpp
@@ -4,6 +4,7 @@
 #include "src/config.h"
 #include "src/logging.h"
 
+#include <algorithm>
 #include <array>
 #include <cerrno>
 #include <chrono>
@@ -12,7 +13,9 @@
 #include <gio/gunixinputstream.h>
 #include <memory>
 #include <mutex>
+#include <optional>
 #include <poll.h>
+#include <string>
 #include <unistd.h>
 
 namespace platf {
@@ -38,34 +41,56 @@ namespace platf {
       }
     };
 
-    std::mutex limiter_mutex;
-    std::array<bool, 2> owners {};
-    std::unique_ptr<limiter_lease> lease;
+    struct owner_policy_t {
+      framegen::stream_start_policy_t stream;
+      proton_color_mode color_mode = proton_color_mode::sdr;
+    };
 
-    std::unique_ptr<limiter_lease> start(const mangohud::launch_policy_t &policy) {
+    std::mutex limiter_mutex;
+    std::array<std::optional<owner_policy_t>, 2> owners {};
+    std::unique_ptr<limiter_lease> lease;
+    std::string active_signature;
+
+    const char *color_mode_name(proton_color_mode color_mode) {
+      switch (color_mode) {
+        case proton_color_mode::hdr:
+          return "hdr";
+        case proton_color_mode::sdr10:
+          return "sdr10";
+        default:
+          return "sdr";
+      }
+    }
+
+    std::unique_ptr<limiter_lease> start(
+      const mangohud::launch_policy_t &policy,
+      proton_color_mode color_mode
+    ) {
       const bool proton = mangohud::proton_provider_selected(config::frame_limiter.provider);
       gchar *mangohud_path = g_find_program_in_path("mangohud");
       const bool available = mangohud_path != nullptr;
       g_free(mangohud_path);
-      if (!proton && !available) {
-        return {};
-      }
-      const bool overlay = available && (!proton || mangohud::proton_overlay_provider_selected(config::frame_limiter.provider));
-      const char *provider = proton ? (overlay ? "mangohud-proton" : "proton") : "mangohud";
-      const std::string limit = std::to_string(policy.limit_millihz);
+      const bool limiter_available = policy.enabled && (proton || available);
+      const bool overlay = limiter_available && available &&
+                           (!proton || mangohud::proton_overlay_provider_selected(config::frame_limiter.provider));
+      const char *provider = !limiter_available ? "disabled" :
+                             proton ? (overlay ? "mangohud-proton" : "proton") : "mangohud";
+      const std::string limit = std::to_string(limiter_available ? policy.limit_millihz : 0);
       const auto &configured_preset = config::frame_limiter.mangohud_preset;
       const char *preset = overlay && (configured_preset == "1" || configured_preset == "2" ||
                                        configured_preset == "3" || configured_preset == "4") ?
                              configured_preset.c_str() :
                              "custom";
       const char *graph = overlay && config::frame_limiter.mangohud_always_show_graph ? "1" : "0";
-      const char *method = !proton && config::frame_limiter.mangohud_limiter_method == "early" ? "early" : "late";
+      const char *method = limiter_available && !proton &&
+                             config::frame_limiter.mangohud_limiter_method == "early" ?
+                             "early" : "late";
       auto result = std::make_unique<limiter_lease>();
       GError *error = nullptr;
       if (std::getenv("VIBESHINE_MACHINE_HOST")) {
-        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibeshine-session-exec", "global-limiter", provider, limit.c_str(), preset, graph, method, nullptr);
+        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibeshine-session-exec", "global-limiter", provider, limit.c_str(), preset, graph, method, color_mode_name(color_mode), nullptr);
       } else {
-        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibeshine-steam-launch", "--global", provider, limit.c_str(), preset, graph, method, "0", "0", nullptr);
+        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibeshine-steam-launch", "--global", provider, limit.c_str(), preset, graph, method, "0", "0", color_mode_name(color_mode), nullptr);
       }
       if (!result->process) {
         BOOST_LOG(warning) << "Global Linux limiter: " << (error ? error->message : "helper unavailable");
@@ -93,8 +118,10 @@ namespace platf {
         }
         if (token == '\n') {
           if (ready_line.starts_with("READY ")) {
-            BOOST_LOG(info) << "Global Linux " << provider << " limiter ready at " << policy.limit
-                            << " FPS for external Proton launches (" << ready_line.substr(6) << " installations).";
+            BOOST_LOG(info) << "Global Linux Proton stream policy ready in " << color_mode_name(color_mode)
+                            << " mode" << (limiter_available ? " with " + std::string(provider) +
+                                                               " limiting at " + policy.limit + " FPS" : "")
+                            << " for external launches (" << ready_line.substr(6) << " installations).";
             return result;
           }
           break;
@@ -104,29 +131,73 @@ namespace platf {
         }
         ready_line += token;
       }
-      BOOST_LOG(warning) << "Global Linux limiter could not prepare external Proton launches; managed launch limiting remains available.";
+      BOOST_LOG(warning) << "Global Linux Proton stream policy could not prepare external launches; managed launches remain available.";
       return {};
+    }
+
+    proton_color_mode aggregate_color_mode() {
+      bool sdr10 = false;
+      for (const auto &owner : owners) {
+        if (!owner) {
+          continue;
+        }
+        if (owner->color_mode == proton_color_mode::hdr) {
+          return proton_color_mode::hdr;
+        }
+        sdr10 |= owner->color_mode == proton_color_mode::sdr10;
+      }
+      return sdr10 ? proton_color_mode::sdr10 : proton_color_mode::sdr;
+    }
+
+    void reconcile() {
+      const auto selected = std::find_if(owners.begin(), owners.end(), [](const auto &owner) {
+        return owner.has_value();
+      });
+      if (selected == owners.end()) {
+        lease.reset();
+        active_signature.clear();
+        return;
+      }
+
+      const auto policy = mangohud::make_launch_policy(
+        config::frame_limiter.provider,
+        config::frame_limiter.enable,
+        config::frame_limiter.virtual_display_limiter_enabled(),
+        (*selected)->stream,
+        config::frame_limiter.fps_limit_millihz
+      );
+      const auto color_mode = aggregate_color_mode();
+      const std::string signature = std::string(color_mode_name(color_mode)) + "|" +
+                                    (policy.enabled ? "1|" + std::to_string(policy.limit_millihz) : "0") + "|" +
+                                    config::frame_limiter.provider + "|" +
+                                    config::frame_limiter.mangohud_preset + "|" +
+                                    (config::frame_limiter.mangohud_always_show_graph ? "1" : "0") + "|" +
+                                    config::frame_limiter.mangohud_limiter_method;
+      if (lease && lease->alive() && signature == active_signature) {
+        return;
+      }
+      lease.reset();
+      active_signature.clear();
+      lease = start(policy, color_mode);
+      if (lease) {
+        active_signature = signature;
+      }
     }
   }  // namespace
 
-  void frame_limiter_streaming_start(frame_limiter_owner owner, const framegen::stream_start_policy_t &stream_policy) {
+  void frame_limiter_streaming_start(
+    frame_limiter_owner owner,
+    const framegen::stream_start_policy_t &stream_policy,
+    proton_color_mode color_mode
+  ) {
     std::lock_guard lock(limiter_mutex);
-    owners[static_cast<std::size_t>(owner)] = true;
-    if (lease && lease->alive()) {
-      return;
-    }
-    lease.reset();
-    const auto policy = mangohud::make_launch_policy(config::frame_limiter.provider, config::frame_limiter.enable, config::frame_limiter.virtual_display_limiter_enabled(), stream_policy, config::frame_limiter.fps_limit_millihz);
-    if (policy.enabled) {
-      lease = start(policy);
-    }
+    owners[static_cast<std::size_t>(owner)] = owner_policy_t {stream_policy, color_mode};
+    reconcile();
   }
 
   void frame_limiter_streaming_stop(frame_limiter_owner owner, bool) {
     std::lock_guard lock(limiter_mutex);
-    owners[static_cast<std::size_t>(owner)] = false;
-    if (!owners[0] && !owners[1]) {
-      lease.reset();
-    }
+    owners[static_cast<std::size_t>(owner)].reset();
+    reconcile();
   }
 }  // namespace platf

tests/unit/platform/linux/test_session_exec_policy.c
@@ -86,20 +86,33 @@ int main(void) {
   CHECK(!steam_direct_arguments_are_safe(11, valid_steam_direct));
 
   char *global_limiter[] = {
-    "vibeshine-session-broker", "global-limiter", "proton", "59940", "custom", "0", "late", NULL
+    "vibeshine-session-broker", "global-limiter", "proton", "59940", "custom", "0", "late", "sdr", NULL
   };
-  CHECK(global_limiter_arguments_are_safe(7, global_limiter));
-  CHECK(!global_limiter_arguments_are_safe(6, global_limiter));
+  CHECK(global_limiter_arguments_are_safe(8, global_limiter));
+  CHECK(!global_limiter_arguments_are_safe(7, global_limiter));
+  global_limiter[7] = "sdr10";
+  CHECK(global_limiter_arguments_are_safe(8, global_limiter));
+  global_limiter[7] = "hdr";
+  CHECK(global_limiter_arguments_are_safe(8, global_limiter));
+  global_limiter[7] = "pq";
+  CHECK(!global_limiter_arguments_are_safe(8, global_limiter));
+  global_limiter[7] = "sdr";
   global_limiter[2] = "mangohud";
   global_limiter[6] = "early";
-  CHECK(global_limiter_arguments_are_safe(7, global_limiter));
+  CHECK(global_limiter_arguments_are_safe(8, global_limiter));
   global_limiter[3] = "59940;touch /tmp/untrusted";
-  CHECK(!global_limiter_arguments_are_safe(7, global_limiter));
+  CHECK(!global_limiter_arguments_are_safe(8, global_limiter));
   global_limiter[3] = "0";
-  CHECK(!global_limiter_arguments_are_safe(7, global_limiter));
+  CHECK(!global_limiter_arguments_are_safe(8, global_limiter));
   global_limiter[3] = "59940";
   global_limiter[4] = "/tmp/preset";
-  CHECK(!global_limiter_arguments_are_safe(7, global_limiter));
+  CHECK(!global_limiter_arguments_are_safe(8, global_limiter));
+  global_limiter[2] = "disabled";
+  global_limiter[3] = "0";
+  global_limiter[4] = "custom";
+  global_limiter[6] = "late";
+  global_limiter[7] = "sdr10";
+  CHECK(global_limiter_arguments_are_safe(8, global_limiter));
 
   CHECK(xauthority_mode_is_safe(0600));
   CHECK(xauthority_mode_is_safe(0400));

Cynary/Vibepollo

(cherry picked from commit 8154f716daccee655dd21d6d3658e43cc712a7fc)

src/platform/linux/frame_limiter.cpp
@@ -4,6 +4,7 @@
 #include "src/config.h"
 #include "src/logging.h"
 
+#include <algorithm>
 #include <array>
 #include <cerrno>
 #include <chrono>
@@ -12,7 +13,9 @@
 #include <gio/gunixinputstream.h>
 #include <memory>
 #include <mutex>
+#include <optional>
 #include <poll.h>
+#include <string>
 #include <unistd.h>
 
 namespace platf {
@@ -38,34 +41,56 @@ namespace platf {
       }
     };
 
-    std::mutex limiter_mutex;
-    std::array<bool, 2> owners {};
-    std::unique_ptr<limiter_lease> lease;
+    struct owner_policy_t {
+      framegen::stream_start_policy_t stream;
+      proton_color_mode color_mode = proton_color_mode::sdr;
+    };
 
-    std::unique_ptr<limiter_lease> start(const mangohud::launch_policy_t &policy) {
+    std::mutex limiter_mutex;
+    std::array<std::optional<owner_policy_t>, 2> owners {};
+    std::unique_ptr<limiter_lease> lease;
+    std::string active_signature;
+
+    const char *color_mode_name(proton_color_mode color_mode) {
+      switch (color_mode) {
+        case proton_color_mode::hdr:
+          return "hdr";
+        case proton_color_mode::sdr10:
+          return "sdr10";
+        default:
+          return "sdr";
+      }
+    }
+
+    std::unique_ptr<limiter_lease> start(
+      const mangohud::launch_policy_t &policy,
+      proton_color_mode color_mode
+    ) {
       const bool proton = mangohud::proton_provider_selected(config::frame_limiter.provider);
       gchar *mangohud_path = g_find_program_in_path("mangohud");
       const bool available = mangohud_path != nullptr;
       g_free(mangohud_path);
-      if (!proton && !available) {
-        return {};
-      }
-      const bool overlay = available && (!proton || mangohud::proton_overlay_provider_selected(config::frame_limiter.provider));
-      const char *provider = proton ? (overlay ? "mangohud-proton" : "proton") : "mangohud";
-      const std::string limit = std::to_string(policy.limit_millihz);
+      const bool limiter_available = policy.enabled && (proton || available);
+      const bool overlay = limiter_available && available &&
+                           (!proton || mangohud::proton_overlay_provider_selected(config::frame_limiter.provider));
+      const char *provider = !limiter_available ? "disabled" :
+                             proton ? (overlay ? "mangohud-proton" : "proton") : "mangohud";
+      const std::string limit = std::to_string(limiter_available ? policy.limit_millihz : 0);
       const auto &configured_preset = config::frame_limiter.mangohud_preset;
       const char *preset = overlay && (configured_preset == "1" || configured_preset == "2" ||
                                        configured_preset == "3" || configured_preset == "4") ?
                              configured_preset.c_str() :
                              "custom";
       const char *graph = overlay && config::frame_limiter.mangohud_always_show_graph ? "1" : "0";
-      const char *method = !proton && config::frame_limiter.mangohud_limiter_method == "early" ? "early" : "late";
+      const char *method = limiter_available && !proton &&
+                             config::frame_limiter.mangohud_limiter_method == "early" ?
+                             "early" : "late";
       auto result = std::make_unique<limiter_lease>();
       GError *error = nullptr;
-      if (std::getenv("VIBESHINE_MACHINE_HOST")) {
-        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibepollo-session-exec", "global-limiter", provider, limit.c_str(), preset, graph, method, nullptr);
+      if (std::getenv("VIBEPOLLO_MACHINE_HOST")) {
+        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibepollo-session-exec", "global-limiter", provider, limit.c_str(), preset, graph, method, color_mode_name(color_mode), nullptr);
       } else {
-        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibepollo-steam-launch", "--global", provider, limit.c_str(), preset, graph, method, "0", "0", nullptr);
+        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibepollo-steam-launch", "--global", provider, limit.c_str(), preset, graph, method, "0", "0", color_mode_name(color_mode), nullptr);
       }
       if (!result->process) {
         BOOST_LOG(warning) << "Global Linux limiter: " << (error ? error->message : "helper unavailable");
@@ -93,8 +118,10 @@ namespace platf {
         }
         if (token == '\n') {
           if (ready_line.starts_with("READY ")) {
-            BOOST_LOG(info) << "Global Linux " << provider << " limiter ready at " << policy.limit
-                            << " FPS for external Proton launches (" << ready_line.substr(6) << " installations).";
+            BOOST_LOG(info) << "Global Linux Proton stream policy ready in " << color_mode_name(color_mode)
+                            << " mode" << (limiter_available ? " with " + std::string(provider) +
+                                                               " limiting at " + policy.limit + " FPS" : "")
+                            << " for external launches (" << ready_line.substr(6) << " installations).";
             return result;
           }
           break;
@@ -104,29 +131,73 @@ namespace platf {
         }
         ready_line += token;
       }
-      BOOST_LOG(warning) << "Global Linux limiter could not prepare external Proton launches; managed launch limiting remains available.";
+      BOOST_LOG(warning) << "Global Linux Proton stream policy could not prepare external launches; managed launches remain available.";
       return {};
+    }
+
+    proton_color_mode aggregate_color_mode() {
+      bool sdr10 = false;
+      for (const auto &owner : owners) {
+        if (!owner) {
+          continue;
+        }
+        if (owner->color_mode == proton_color_mode::hdr) {
+          return proton_color_mode::hdr;
+        }
+        sdr10 |= owner->color_mode == proton_color_mode::sdr10;
+      }
+      return sdr10 ? proton_color_mode::sdr10 : proton_color_mode::sdr;
+    }
+
+    void reconcile() {
+      const auto selected = std::find_if(owners.begin(), owners.end(), [](const auto &owner) {
+        return owner.has_value();
+      });
+      if (selected == owners.end()) {
+        lease.reset();
+        active_signature.clear();
+        return;
+      }
+
+      const auto policy = mangohud::make_launch_policy(
+        config::frame_limiter.provider,
+        config::frame_limiter.enable,
+        config::frame_limiter.virtual_display_limiter_enabled(),
+        (*selected)->stream,
+        config::frame_limiter.fps_limit_millihz
+      );
+      const auto color_mode = aggregate_color_mode();
+      const std::string signature = std::string(color_mode_name(color_mode)) + "|" +
+                                    (policy.enabled ? "1|" + std::to_string(policy.limit_millihz) : "0") + "|" +
+                                    config::frame_limiter.provider + "|" +
+                                    config::frame_limiter.mangohud_preset + "|" +
+                                    (config::frame_limiter.mangohud_always_show_graph ? "1" : "0") + "|" +
+                                    config::frame_limiter.mangohud_limiter_method;
+      if (lease && lease->alive() && signature == active_signature) {
+        return;
+      }
+      lease.reset();
+      active_signature.clear();
+      lease = start(policy, color_mode);
+      if (lease) {
+        active_signature = signature;
+      }
     }
   }  // namespace
 
-  void frame_limiter_streaming_start(frame_limiter_owner owner, const framegen::stream_start_policy_t &stream_policy) {
+  void frame_limiter_streaming_start(
+    frame_limiter_owner owner,
+    const framegen::stream_start_policy_t &stream_policy,
+    proton_color_mode color_mode
+  ) {
     std::lock_guard lock(limiter_mutex);
-    owners[static_cast<std::size_t>(owner)] = true;
-    if (lease && lease->alive()) {
-      return;
-    }
-    lease.reset();
-    const auto policy = mangohud::make_launch_policy(config::frame_limiter.provider, config::frame_limiter.enable, config::frame_limiter.virtual_display_limiter_enabled(), stream_policy, config::frame_limiter.fps_limit_millihz);
-    if (policy.enabled) {
-      lease = start(policy);
-    }
+    owners[static_cast<std::size_t>(owner)] = owner_policy_t {stream_policy, color_mode};
+    reconcile();
   }
 
   void frame_limiter_streaming_stop(frame_limiter_owner owner, bool) {
     std::lock_guard lock(limiter_mutex);
-    owners[static_cast<std::size_t>(owner)] = false;
-    if (!owners[0] && !owners[1]) {
-      lease.reset();
-    }
+    owners[static_cast<std::size_t>(owner)].reset();
+    reconcile();
   }
 }  // namespace platf

tests/unit/platform/linux/test_session_exec_policy.c
@@ -50,7 +50,7 @@ int main(void) {
   CHECK(parse_number("10", 1, 10, &number) && number == 10);
 
   char *valid_steam_direct[] = {
-    "vibeshine-session-broker", "steam-direct", "1182900",
+    "vibepollo-session-broker", "steam-direct", "1182900",
     "mangohud-proton", "116000", "3", "1", "late", "0", "0", "1", NULL
   };
   CHECK(steam_direct_arguments_are_safe(11, valid_steam_direct));
@@ -86,20 +86,33 @@ int main(void) {
   CHECK(!steam_direct_arguments_are_safe(11, valid_steam_direct));
 
   char *global_limiter[] = {
-    "vibeshine-session-broker", "global-limiter", "proton", "59940", "custom", "0", "late", NULL
+    "vibepollo-session-broker", "global-limiter", "proton", "59940", "custom", "0", "late", "sdr", NULL
   };
-  CHECK(global_limiter_arguments_are_safe(7, global_limiter));
-  CHECK(!global_limiter_arguments_are_safe(6, global_limiter));
+  CHECK(global_limiter_arguments_are_safe(8, global_limiter));
+  CHECK(!global_limiter_arguments_are_safe(7, global_limiter));
+  global_limiter[7] = "sdr10";
+  CHECK(global_limiter_arguments_are_safe(8, global_limiter));
+  global_limiter[7] = "hdr";
+  CHECK(global_limiter_arguments_are_safe(8, global_limiter));
+  global_limiter[7] = "pq";
+  CHECK(!global_limiter_arguments_are_safe(8, global_limiter));
+  global_limiter[7] = "sdr";
   global_limiter[2] = "mangohud";
   global_limiter[6] = "early";
-  CHECK(global_limiter_arguments_are_safe(7, global_limiter));
+  CHECK(global_limiter_arguments_are_safe(8, global_limiter));
   global_limiter[3] = "59940;touch /tmp/untrusted";
-  CHECK(!global_limiter_arguments_are_safe(7, global_limiter));
+  CHECK(!global_limiter_arguments_are_safe(8, global_limiter));
   global_limiter[3] = "0";
-  CHECK(!global_limiter_arguments_are_safe(7, global_limiter));
+  CHECK(!global_limiter_arguments_are_safe(8, global_limiter));
   global_limiter[3] = "59940";
   global_limiter[4] = "/tmp/preset";
-  CHECK(!global_limiter_arguments_are_safe(7, global_limiter));
+  CHECK(!global_limiter_arguments_are_safe(8, global_limiter));
+  global_limiter[2] = "disabled";
+  global_limiter[3] = "0";
+  global_limiter[4] = "custom";
+  global_limiter[6] = "late";
+  global_limiter[7] = "sdr10";
+  CHECK(global_limiter_arguments_are_safe(8, global_limiter));
 
   CHECK(xauthority_mode_is_safe(0600));
   CHECK(xauthority_mode_is_safe(0400));