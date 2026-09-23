Nonary/vibeshine

feat(linux): apply HDR compatibility to Wayland games
Expose the resolved HDR stream state to direct launches and Proton games started through an existing Steam session.

Keep the policy scoped to active HDR streams, preserve explicit launch overrides, and suppress Wayland HDR variables for SDR outcomes. Add matching v1 and v2 controls plus launch-policy coverage.

A live Cyberpunk launch with GE-Proton confirmed the corrected HDR path.

Generated with [Codex](https://openai.com/codex/)
Model: GPT 5.6-Sol High

src/platform/linux/frame_limiter.cpp
@@ -43,11 +43,12 @@ namespace platf {
 
     struct owner_policy_t {
       framegen::stream_start_policy_t stream;
-      proton_color_mode color_mode = proton_color_mode::sdr;
+      proton_launch_environment_t environment;
     };
 
     std::mutex limiter_mutex;
-    std::array<std::optional<owner_policy_t>, 2> owners {};
+    std::array<std::optional<owner_policy_t>,
+               static_cast<std::size_t>(frame_limiter_owner::application) + 1> owners {};
     std::unique_ptr<limiter_lease> lease;
     std::string active_signature;
 
@@ -64,7 +65,7 @@ namespace platf {
 
     std::unique_ptr<limiter_lease> start(
       const mangohud::launch_policy_t &policy,
-      proton_color_mode color_mode
+      const proton_launch_environment_t environment
     ) {
       const bool proton = mangohud::proton_provider_selected(config::frame_limiter.provider);
       gchar *mangohud_path = g_find_program_in_path("mangohud");
@@ -88,9 +89,9 @@ namespace platf {
       auto result = std::make_unique<limiter_lease>();
       GError *error = nullptr;
       if (std::getenv("VIBESHINE_MACHINE_HOST")) {
-        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibeshine-session-exec", "global-limiter", provider, limit.c_str(), preset, graph, method, color_mode_name(color_mode), nullptr);
+        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibeshine-session-exec", "global-limiter", provider, limit.c_str(), preset, graph, method, color_mode_name(environment.color_mode), environment.wayland_hdr_compatibility ? "1" : "0", nullptr);
       } else {
-        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibeshine-steam-launch", "--global", provider, limit.c_str(), preset, graph, method, "0", "0", color_mode_name(color_mode), nullptr);
+        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibeshine-steam-launch", "--global", provider, limit.c_str(), preset, graph, method, "0", "0", color_mode_name(environment.color_mode), environment.wayland_hdr_compatibility ? "1" : "0", nullptr);
       }
       if (!result->process) {
         BOOST_LOG(warning) << "Global Linux limiter: " << (error ? error->message : "helper unavailable");
@@ -118,10 +119,14 @@ namespace platf {
         }
         if (token == '\n') {
           if (ready_line.starts_with("READY ")) {
-            BOOST_LOG(info) << "Global Linux Proton stream policy ready in " << color_mode_name(color_mode)
+            BOOST_LOG(info) << "Global Linux Proton stream policy ready in " << color_mode_name(environment.color_mode)
                             << " mode" << (limiter_available ? " with " + std::string(provider) +
                                                                " limiting at " + policy.limit + " FPS" : "")
                             << " for external launches (" << ready_line.substr(6) << " installations).";
+            if (environment.wayland_hdr_compatibility) {
+              BOOST_LOG(info) << "Wayland HDR compatibility enabled for external Proton application launches.";
+              BOOST_LOG(debug) << "Wayland HDR compatibility flags applied through the session-owned Proton hook.";
+            }
             return result;
           }
           break;
@@ -135,18 +140,27 @@ namespace platf {
       return {};
     }
 
-    proton_color_mode aggregate_color_mode() {
+    proton_launch_environment_t aggregate_environment() {
       bool sdr10 = false;
+      bool hdr = false;
+      bool wayland_hdr_compatibility = false;
       for (const auto &owner : owners) {
         if (!owner) {
           continue;
         }
-        if (owner->color_mode == proton_color_mode::hdr) {
-          return proton_color_mode::hdr;
+        if (owner->environment.color_mode == proton_color_mode::hdr) {
+          hdr = true;
+          wayland_hdr_compatibility |= owner->environment.wayland_hdr_compatibility;
+          continue;
         }
-        sdr10 |= owner->color_mode == proton_color_mode::sdr10;
+        sdr10 |= owner->environment.color_mode == proton_color_mode::sdr10;
       }
-      return sdr10 ? proton_color_mode::sdr10 : proton_color_mode::sdr;
+      if (hdr) {
+        return {.color_mode = proton_color_mode::hdr,
+                .wayland_hdr_compatibility = wayland_hdr_compatibility};
+      }
+      return {.color_mode = sdr10 ? proton_color_mode::sdr10 : proton_color_mode::sdr,
+              .wayland_hdr_compatibility = false};
     }
 
     void reconcile() {
@@ -166,8 +180,9 @@ namespace platf {
         (*selected)->stream,
         config::frame_limiter.fps_limit_millihz
       );
-      const auto color_mode = aggregate_color_mode();
-      const std::string signature = std::string(color_mode_name(color_mode)) + "|" +
+      const auto environment = aggregate_environment();
+      const std::string signature = std::string(color_mode_name(environment.color_mode)) + "|" +
+                                    (environment.wayland_hdr_compatibility ? "1|" : "0|") +
                                     (policy.enabled ? "1|" + std::to_string(policy.limit_millihz) : "0") + "|" +
                                     config::frame_limiter.provider + "|" +
                                     config::frame_limiter.mangohud_preset + "|" +
@@ -178,7 +193,7 @@ namespace platf {
       }
       lease.reset();
       active_signature.clear();
-      lease = start(policy, color_mode);
+      lease = start(policy, environment);
       if (lease) {
         active_signature = signature;
       }
@@ -188,10 +203,10 @@ namespace platf {
   void frame_limiter_streaming_start(
     frame_limiter_owner owner,
     const framegen::stream_start_policy_t &stream_policy,
-    proton_color_mode color_mode
+    proton_launch_environment_t environment
   ) {
     std::lock_guard lock(limiter_mutex);
-    owners[static_cast<std::size_t>(owner)] = owner_policy_t {stream_policy, color_mode};
+    owners[static_cast<std::size_t>(owner)] = owner_policy_t {stream_policy, environment};
     reconcile();
   }
 

src/platform/linux/wayland_hdr_compatibility.h
@@ -1,0 +1,66 @@
+/**
+ * @file src/platform/linux/wayland_hdr_compatibility.h
+ * @brief Resolved-session gate for the optional native Wayland HDR launch policy.
+ */
+#pragma once
+
+#include <cstdlib>
+#include <string_view>
+
+namespace platf::wayland_hdr_compatibility {
+  enum class suppression_reason_e {
+    none,
+    disabled,
+    not_wayland,
+    resolved_sdr,
+  };
+
+  struct decision_t {
+    bool enabled {};
+    suppression_reason_e suppression_reason {suppression_reason_e::disabled};
+  };
+
+  /**
+   * The machine-scoped host deliberately runs with neither WAYLAND_DISPLAY nor
+   * DISPLAY because it captures through KMS and delegates desktop launches to
+   * the selected session. In that architecture, the trusted controller must
+   * describe the selected session separately from the host process's own
+   * window-system access.
+   */
+  constexpr bool selected_session_is_wayland(
+    bool process_uses_wayland,
+    std::string_view managed_session_type
+  ) {
+    return process_uses_wayland || managed_session_type == "wayland";
+  }
+
+  inline bool selected_session_is_wayland(bool process_uses_wayland) {
+    const char *managed_session_type = std::getenv("VIBESHINE_SESSION_TYPE");
+    return selected_session_is_wayland(
+      process_uses_wayland,
+      managed_session_type ? std::string_view {managed_session_type} : std::string_view {}
+    );
+  }
+
+  /**
+   * The caller supplies the already-resolved stream mode. In particular, a
+   * 10-bit SDR stream and a requested-but-unavailable HDR output are both
+   * resolved_sdr: neither is evidence that a game should expose HDR.
+   */
+  constexpr decision_t resolve(
+    bool configured,
+    bool wayland_session,
+    bool resolved_hdr
+  ) {
+    if (!configured) {
+      return {.enabled = false, .suppression_reason = suppression_reason_e::disabled};
+    }
+    if (!wayland_session) {
+      return {.enabled = false, .suppression_reason = suppression_reason_e::not_wayland};
+    }
+    if (!resolved_hdr) {
+      return {.enabled = false, .suppression_reason = suppression_reason_e::resolved_sdr};
+    }
+    return {.enabled = true, .suppression_reason = suppression_reason_e::none};
+  }
+}  // namespace platf::wayland_hdr_compatibility

Cynary/Vibepollo

(cherry picked from commit aad2d82b1f4f487cdd17035efa51f576b3ff9436)

src/platform/linux/frame_limiter.cpp
@@ -43,11 +43,12 @@ namespace platf {
 
     struct owner_policy_t {
       framegen::stream_start_policy_t stream;
-      proton_color_mode color_mode = proton_color_mode::sdr;
+      proton_launch_environment_t environment;
     };
 
     std::mutex limiter_mutex;
-    std::array<std::optional<owner_policy_t>, 2> owners {};
+    std::array<std::optional<owner_policy_t>,
+               static_cast<std::size_t>(frame_limiter_owner::application) + 1> owners {};
     std::unique_ptr<limiter_lease> lease;
     std::string active_signature;
 
@@ -64,7 +65,7 @@ namespace platf {
 
     std::unique_ptr<limiter_lease> start(
       const mangohud::launch_policy_t &policy,
-      proton_color_mode color_mode
+      const proton_launch_environment_t environment
     ) {
       const bool proton = mangohud::proton_provider_selected(config::frame_limiter.provider);
       gchar *mangohud_path = g_find_program_in_path("mangohud");
@@ -88,9 +89,9 @@ namespace platf {
       auto result = std::make_unique<limiter_lease>();
       GError *error = nullptr;
       if (std::getenv("VIBEPOLLO_MACHINE_HOST")) {
-        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibepollo-session-exec", "global-limiter", provider, limit.c_str(), preset, graph, method, color_mode_name(color_mode), nullptr);
+        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibepollo-session-exec", "global-limiter", provider, limit.c_str(), preset, graph, method, color_mode_name(environment.color_mode), environment.wayland_hdr_compatibility ? "1" : "0", nullptr);
       } else {
-        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibepollo-steam-launch", "--global", provider, limit.c_str(), preset, graph, method, "0", "0", color_mode_name(color_mode), nullptr);
+        result->process = g_subprocess_new(G_SUBPROCESS_FLAGS_STDOUT_PIPE, &error, "/usr/libexec/vibeshine/vibepollo-steam-launch", "--global", provider, limit.c_str(), preset, graph, method, "0", "0", color_mode_name(environment.color_mode), environment.wayland_hdr_compatibility ? "1" : "0", nullptr);
       }
       if (!result->process) {
         BOOST_LOG(warning) << "Global Linux limiter: " << (error ? error->message : "helper unavailable");
@@ -118,10 +119,14 @@ namespace platf {
         }
         if (token == '\n') {
           if (ready_line.starts_with("READY ")) {
-            BOOST_LOG(info) << "Global Linux Proton stream policy ready in " << color_mode_name(color_mode)
+            BOOST_LOG(info) << "Global Linux Proton stream policy ready in " << color_mode_name(environment.color_mode)
                             << " mode" << (limiter_available ? " with " + std::string(provider) +
                                                                " limiting at " + policy.limit + " FPS" : "")
                             << " for external launches (" << ready_line.substr(6) << " installations).";
+            if (environment.wayland_hdr_compatibility) {
+              BOOST_LOG(info) << "Wayland HDR compatibility enabled for external Proton application launches.";
+              BOOST_LOG(debug) << "Wayland HDR compatibility flags applied through the session-owned Proton hook.";
+            }
             return result;
           }
           break;
@@ -135,18 +140,27 @@ namespace platf {
       return {};
     }
 
-    proton_color_mode aggregate_color_mode() {
+    proton_launch_environment_t aggregate_environment() {
       bool sdr10 = false;
+      bool hdr = false;
+      bool wayland_hdr_compatibility = false;
       for (const auto &owner : owners) {
         if (!owner) {
           continue;
         }
-        if (owner->color_mode == proton_color_mode::hdr) {
-          return proton_color_mode::hdr;
+        if (owner->environment.color_mode == proton_color_mode::hdr) {
+          hdr = true;
+          wayland_hdr_compatibility |= owner->environment.wayland_hdr_compatibility;
+          continue;
         }
-        sdr10 |= owner->color_mode == proton_color_mode::sdr10;
+        sdr10 |= owner->environment.color_mode == proton_color_mode::sdr10;
       }
-      return sdr10 ? proton_color_mode::sdr10 : proton_color_mode::sdr;
+      if (hdr) {
+        return {.color_mode = proton_color_mode::hdr,
+                .wayland_hdr_compatibility = wayland_hdr_compatibility};
+      }
+      return {.color_mode = sdr10 ? proton_color_mode::sdr10 : proton_color_mode::sdr,
+              .wayland_hdr_compatibility = false};
     }
 
     void reconcile() {
@@ -166,8 +180,9 @@ namespace platf {
         (*selected)->stream,
         config::frame_limiter.fps_limit_millihz
       );
-      const auto color_mode = aggregate_color_mode();
-      const std::string signature = std::string(color_mode_name(color_mode)) + "|" +
+      const auto environment = aggregate_environment();
+      const std::string signature = std::string(color_mode_name(environment.color_mode)) + "|" +
+                                    (environment.wayland_hdr_compatibility ? "1|" : "0|") +
                                     (policy.enabled ? "1|" + std::to_string(policy.limit_millihz) : "0") + "|" +
                                     config::frame_limiter.provider + "|" +
                                     config::frame_limiter.mangohud_preset + "|" +
@@ -178,7 +193,7 @@ namespace platf {
       }
       lease.reset();
       active_signature.clear();
-      lease = start(policy, color_mode);
+      lease = start(policy, environment);
       if (lease) {
         active_signature = signature;
       }
@@ -188,10 +203,10 @@ namespace platf {
   void frame_limiter_streaming_start(
     frame_limiter_owner owner,
     const framegen::stream_start_policy_t &stream_policy,
-    proton_color_mode color_mode
+    proton_launch_environment_t environment
   ) {
     std::lock_guard lock(limiter_mutex);
-    owners[static_cast<std::size_t>(owner)] = owner_policy_t {stream_policy, color_mode};
+    owners[static_cast<std::size_t>(owner)] = owner_policy_t {stream_policy, environment};
     reconcile();
   }
 

src/platform/linux/wayland_hdr_compatibility.h
@@ -1,0 +1,66 @@
+/**
+ * @file src/platform/linux/wayland_hdr_compatibility.h
+ * @brief Resolved-session gate for the optional native Wayland HDR launch policy.
+ */
+#pragma once
+
+#include <cstdlib>
+#include <string_view>
+
+namespace platf::wayland_hdr_compatibility {
+  enum class suppression_reason_e {
+    none,
+    disabled,
+    not_wayland,
+    resolved_sdr,
+  };
+
+  struct decision_t {
+    bool enabled {};
+    suppression_reason_e suppression_reason {suppression_reason_e::disabled};
+  };
+
+  /**
+   * The machine-scoped host deliberately runs with neither WAYLAND_DISPLAY nor
+   * DISPLAY because it captures through KMS and delegates desktop launches to
+   * the selected session. In that architecture, the trusted controller must
+   * describe the selected session separately from the host process's own
+   * window-system access.
+   */
+  constexpr bool selected_session_is_wayland(
+    bool process_uses_wayland,
+    std::string_view managed_session_type
+  ) {
+    return process_uses_wayland || managed_session_type == "wayland";
+  }
+
+  inline bool selected_session_is_wayland(bool process_uses_wayland) {
+    const char *managed_session_type = std::getenv("VIBEPOLLO_SESSION_TYPE");
+    return selected_session_is_wayland(
+      process_uses_wayland,
+      managed_session_type ? std::string_view {managed_session_type} : std::string_view {}
+    );
+  }
+
+  /**
+   * The caller supplies the already-resolved stream mode. In particular, a
+   * 10-bit SDR stream and a requested-but-unavailable HDR output are both
+   * resolved_sdr: neither is evidence that a game should expose HDR.
+   */
+  constexpr decision_t resolve(
+    bool configured,
+    bool wayland_session,
+    bool resolved_hdr
+  ) {
+    if (!configured) {
+      return {.enabled = false, .suppression_reason = suppression_reason_e::disabled};
+    }
+    if (!wayland_session) {
+      return {.enabled = false, .suppression_reason = suppression_reason_e::not_wayland};
+    }
+    if (!resolved_hdr) {
+      return {.enabled = false, .suppression_reason = suppression_reason_e::resolved_sdr};
+    }
+    return {.enabled = true, .suppression_reason = suppression_reason_e::none};
+  }
+}  // namespace platf::wayland_hdr_compatibility