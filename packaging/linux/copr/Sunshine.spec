Nonary/vibeshine

fix(linux): drain GPU host before tearing down broker clients
Service-wide TERM could cancel session apps while capture still held GPU resources.
Forward shutdown once to the host, then retire remaining clients with pidfds.
Keep brokers alive when host draining fails and preserve upgrade compatibility.
Cover signal races and enforce ordered cleanup across package lifecycle hooks.

Generated with [Codex](https://openai.com/codex/)
Model: GPT 6-Astra Ultra

packaging/linux/copr/Sunshine.spec
@@ -148,6 +148,7 @@ Requires: iproute
 Requires: jq
 Requires: /usr/bin/pactl
 Requires: /usr/bin/parec
+Requires: /usr/bin/python3
 Requires: /usr/bin/wayland-info
 Requires: /usr/bin/xdpyinfo
 Requires: socat
@@ -159,6 +160,7 @@ Recommends: make
 
 %if 0%{?fedora}
 # Fedora runtime requirements
+Requires: python3 >= 3.9
 Requires: libayatana-appindicator3 >= 0.5.3
 Requires: libcap >= 2.22
 Requires: libcurl >= 7.0
@@ -177,6 +179,7 @@ Requires: vulkan-loader
 
 %if 0%{?suse_version}
 # OpenSUSE runtime requirements
+Requires: python311
 Requires: libappindicator3-1
 Requires: libcap2
 Requires: libcurl4
@@ -654,7 +657,9 @@ vibeshine_select_upgrade_kill_mode() {
     return 0
   fi
   vibeshine_privileged_helper_is_safe "$vibeshine_legacy_host" || return 1
-  if grep -Fqx '  trap mark_host_shutdown TERM INT HUP' "$vibeshine_legacy_host"; then
+  if grep -Fqx '  trap request_host_shutdown TERM INT HUP' "$vibeshine_legacy_host"; then
+    vibeshine_upgrade_kill_mode=mixed
+  elif grep -Fqx '  trap mark_host_shutdown TERM INT HUP' "$vibeshine_legacy_host"; then
     vibeshine_upgrade_kill_mode=control-group
   elif grep -Fqx "  trap 'forward_host_signal TERM' TERM" "$vibeshine_legacy_host" && \
        grep -Fqx "  trap 'forward_host_signal INT' INT" "$vibeshine_legacy_host" && \
@@ -672,7 +677,7 @@ vibeshine_prepare_host_upgrade_fence() (
     '0:0:755:directory' ] || exit 1
   vibeshine_upgrade_temporary=$(mktemp /run/vibeshine-host-upgrade.XXXXXX) || exit 1
   trap 'rm -f -- "$vibeshine_upgrade_temporary"' 0
-  case "$vibeshine_upgrade_kill_mode" in process | control-group) ;; *) exit 1 ;; esac
+  case "$vibeshine_upgrade_kill_mode" in mixed | process | control-group) ;; *) exit 1 ;; esac
   printf '[Unit]\nRefuseManualStart=yes\n\n[Service]\nKillMode=%%s\nSendSIGKILL=no\n' \
     "$vibeshine_upgrade_kill_mode" >"$vibeshine_upgrade_temporary" || exit 1
   chmod 0600 -- "$vibeshine_upgrade_temporary" || exit 1
@@ -1038,6 +1043,12 @@ RefuseManualStart=yes
 
 [Service]
 KillMode=control-group
+SendSIGKILL=no' | \
+      '[Unit]
+RefuseManualStart=yes
+
+[Service]
+KillMode=mixed
 SendSIGKILL=no') ;;
       *) return 1 ;;
     esac
@@ -1050,7 +1061,7 @@ SendSIGKILL=no') ;;
   vibeshine_new_host_properties=$(timeout --signal=KILL 5 systemctl show vibeshine.service \
     --property=RefuseManualStart --property=KillMode --property=SendSIGKILL 2>/dev/null) || return 1
   printf '%%s\n' "$vibeshine_new_host_properties" | grep -qx 'RefuseManualStart=no' && \
-    printf '%%s\n' "$vibeshine_new_host_properties" | grep -qx 'KillMode=control-group' && \
+    printf '%%s\n' "$vibeshine_new_host_properties" | grep -qx 'KillMode=mixed' && \
     printf '%%s\n' "$vibeshine_new_host_properties" | grep -qx 'SendSIGKILL=no' || return 1
   timeout --signal=KILL 15 systemctl unmask --runtime vibeshine-vkms-control.socket 2>/dev/null || return 1
   systemctl start vibeshine-vkms-control.socket || return 1
@@ -1209,6 +1220,8 @@ vibeshine_quiesce_machine_host() {
   vibeshine_unit_is_masked vibeshine-session-exec.socket || return 1
   vibeshine_stop_exact_unit vibeshine-session-exec.socket
   vibeshine_unit_is_quiescent vibeshine-session-exec.socket || return 1
+  vibeshine_stop_exact_unit vibeshine.service
+  vibeshine_unit_is_quiescent vibeshine.service || return 1
   vibeshine_stop_brokers || return 1
   for vibeshine_unit in vibeshine-session-controller.service vibeshine.service \
     vibeshine-prelogin.service vibeshine-machine-prepare.service; do
@@ -1225,6 +1238,8 @@ vibeshine_quiesce_machine_host() {
   vibeshine_unit_is_masked vibeshine.service || return 1
   vibeshine_unit_is_masked vibeshine-session-exec.socket || return 1
   vibeshine_stop_exact_unit vibeshine-session-exec.socket
+  vibeshine_stop_exact_unit vibeshine.service
+  vibeshine_unit_is_quiescent vibeshine.service || return 1
   vibeshine_stop_brokers || return 1
   for vibeshine_unit in vibeshine-session-exec.socket vibeshine-session-controller.service \
     vibeshine.service vibeshine-prelogin.service vibeshine-machine-prepare.service; do
@@ -1559,6 +1574,8 @@ vibeshine_preun_quiesce() {
     vibeshine_preun_unit_is_masked vibeshine-session-exec.socket || return 1
     vibeshine_preun_stop_exact_unit vibeshine-session-exec.socket
     vibeshine_preun_unit_is_quiescent vibeshine-session-exec.socket || return 1
+    vibeshine_preun_stop_exact_unit vibeshine.service
+    vibeshine_preun_unit_is_quiescent vibeshine.service || return 1
     vibeshine_preun_stop_brokers || return 1
     for vibeshine_unit in vibeshine-session-controller.service vibeshine.service \
       vibeshine-prelogin.service vibeshine-machine-prepare.service; do
@@ -1579,6 +1596,8 @@ vibeshine_preun_quiesce() {
     vibeshine_preun_unit_is_masked vibeshine.service || return 1
     vibeshine_preun_unit_is_masked vibeshine-session-exec.socket || return 1
     vibeshine_preun_stop_exact_unit vibeshine-session-exec.socket
+    vibeshine_preun_stop_exact_unit vibeshine.service
+    vibeshine_preun_unit_is_quiescent vibeshine.service || return 1
     vibeshine_preun_stop_brokers || return 1
     for vibeshine_unit in vibeshine-session-exec.socket vibeshine-session-controller.service \
       vibeshine.service vibeshine-prelogin.service vibeshine-machine-prepare.service; do

tests/unit/platform/linux/test_machine_service_contract.py
@@ -193,7 +193,7 @@ require(
 )
 for forbidden in ("ExecStart=", "ExecStartPre=", "WantedBy="):
     forbid(kwin_environment_dropin, forbidden, "KWin session environment drop-in")
-require(controller, 'stop_host "$deadline" || success=1', "fail-closed transition ordering")
+require(controller, 'stop_host "$deadline" || return 1', "fail-closed transition ordering")
 require(controller, "binding_matches_candidate complete", "display credential rebinding")
 require(controller, "observe_active_session || return 1", "authoritative transition snapshot")
 require(controller, "read_user_environment || return 1", "current transition credentials")
@@ -224,7 +224,7 @@ quiesce_body = controller.split("\nquiesce() {\n", 1)[1].split("\n}\n\nnext_gene
 quiesce_normalized = " ".join(quiesce_body.split())
 quiesce_order = (
     quiesce_normalized.index('close_broker_admission "$deadline" || return 1'),
-    quiesce_normalized.index('stop_host "$deadline" || success=1'),
+    quiesce_normalized.index('stop_host "$deadline" || return 1'),
     quiesce_normalized.index('stop_broker_instances "$deadline" || success=1'),
     quiesce_normalized.index('stop_bound_session_apps "$deadline" || success=1'),
     quiesce_normalized.rindex('stop_broker_instances "$deadline" || success=1'),
@@ -288,7 +288,7 @@ for unit_text, label in (
     forbid(unit_text, "PartOf=vibeshine-session-controller.service", f"{label} ordered controller cleanup")
 require(broker_unit.split("[Service]", 1)[0], "CollectMode=inactive-or-failed",
         "completed rejected broker requests must not exhaust handoff inventory")
-require(host_unit, "KillMode=control-group", "machine host process-tree shutdown")
+require(host_unit, "KillMode=mixed", "supervisor-owned host shutdown")
 require(host_unit, "SendSIGKILL=no", "GPU-owner graceful shutdown")
 require(host_unit, "Type=notify", "encoder-gated machine host readiness")
 require(host_unit, "NotifyAccess=all", "encoder-gated machine host readiness")
@@ -305,13 +305,12 @@ require(host_unit, "ProtectHome=yes", "machine host unit")
 require(host_unit, "PrivateTmp=yes", "machine host unit")
 require(host_unit, "RestrictSUIDSGID=yes", "machine host unit")
 for forbidden in (
-    "User=root", "CAP_DAC_OVERRIDE", "machine-prepare", "ExecStopPost=", "WantedBy=", "KillMode=mixed",
+    "User=root", "CAP_DAC_OVERRIDE", "machine-prepare", "ExecStopPost=", "WantedBy=", "KillMode=control-group",
     "BindsTo=vibeshine-session-controller.service",
 ):
     forbid(host_unit_directives, forbidden, "machine host unit")
-require(host, "trap mark_host_shutdown TERM INT HUP", "single service signal delivery")
+require(host, "trap request_host_shutdown TERM INT HUP", "single service signal delivery")
 forbid(host, "trap 'forward_host_signal", "duplicate service signal delivery")
-require(host, "((shutting_down)) || terminate_host", "single child termination request")
 
 # API restart of the private child exits back to the readiness-gating wrapper.
 # Ordinary Linux launches retain the historical atexit self-reexec path.
@@ -773,6 +772,8 @@ for lifecycle, label in (
         "RefuseManualStart=yes",
         "vibeshine_upgrade_kill_mode=process",
         "vibeshine_upgrade_kill_mode=control-group",
+        "vibeshine_upgrade_kill_mode=mixed",
+        "trap request_host_shutdown TERM INT HUP",
         "SendSIGKILL=no",
         "--property=Job",
         "systemctl freeze",
@@ -923,7 +924,7 @@ for lifecycle, label in (
         "vibeshine_unmask_host_for_controller",
         "90-vibeshine-safe-upgrade.conf",
         "RefuseManualStart=no",
-        "KillMode=control-group",
+        "KillMode=mixed",
         "SendSIGKILL=no",
         "unmask --runtime vibeshine-vkms-control.socket",
         "start vibeshine-vkms-control.socket",

Cynary/Vibepollo

(cherry picked from commit 92ab9c4035400422e1d9658155cd8869b17e28c5)

packaging/linux/copr/Sunshine.spec
@@ -150,6 +150,7 @@ Requires: jq
 Requires: python3
 Requires: /usr/bin/pactl
 Requires: /usr/bin/parec
+Requires: /usr/bin/python3
 Requires: /usr/bin/wayland-info
 Requires: /usr/bin/xdpyinfo
 Requires: socat
@@ -161,6 +162,7 @@ Recommends: make
 
 %if 0%{?fedora}
 # Fedora runtime requirements
+Requires: python3 >= 3.9
 Requires: libayatana-appindicator3 >= 0.5.3
 Requires: libcap >= 2.22
 Requires: libcurl >= 7.0
@@ -179,6 +181,7 @@ Requires: vulkan-loader
 
 %if 0%{?suse_version}
 # OpenSUSE runtime requirements
+Requires: python311
 Requires: libappindicator3-1
 Requires: libcap2
 Requires: libcurl4
@@ -653,7 +656,9 @@ vibepollo_select_upgrade_kill_mode() {
     return 0
   fi
   vibepollo_privileged_helper_is_safe "$vibepollo_legacy_host" || return 1
-  if grep -Fqx '  trap mark_host_shutdown TERM INT HUP' "$vibepollo_legacy_host"; then
+  if grep -Fqx '  trap request_host_shutdown TERM INT HUP' "$vibepollo_legacy_host"; then
+    vibepollo_upgrade_kill_mode=mixed
+  elif grep -Fqx '  trap mark_host_shutdown TERM INT HUP' "$vibepollo_legacy_host"; then
     vibepollo_upgrade_kill_mode=control-group
   elif grep -Fqx "  trap 'forward_host_signal TERM' TERM" "$vibepollo_legacy_host" && \
        grep -Fqx "  trap 'forward_host_signal INT' INT" "$vibepollo_legacy_host" && \
@@ -671,7 +676,7 @@ vibepollo_prepare_host_upgrade_fence() (
     '0:0:755:directory' ] || exit 1
   vibepollo_upgrade_temporary=$(mktemp /run/vibepollo-host-upgrade.XXXXXX) || exit 1
   trap 'rm -f -- "$vibepollo_upgrade_temporary"' 0
-  case "$vibepollo_upgrade_kill_mode" in process | control-group) ;; *) exit 1 ;; esac
+  case "$vibepollo_upgrade_kill_mode" in mixed | process | control-group) ;; *) exit 1 ;; esac
   printf '[Unit]\nRefuseManualStart=yes\n\n[Service]\nKillMode=%%s\nSendSIGKILL=no\n' \
     "$vibepollo_upgrade_kill_mode" >"$vibepollo_upgrade_temporary" || exit 1
   chmod 0600 -- "$vibepollo_upgrade_temporary" || exit 1
@@ -1037,6 +1042,12 @@ RefuseManualStart=yes
 
 [Service]
 KillMode=control-group
+SendSIGKILL=no' | \
+      '[Unit]
+RefuseManualStart=yes
+
+[Service]
+KillMode=mixed
 SendSIGKILL=no') ;;
       *) return 1 ;;
     esac
@@ -1049,7 +1060,7 @@ SendSIGKILL=no') ;;
   vibepollo_new_host_properties=$(timeout --signal=KILL 5 systemctl show vibepollo.service \
     --property=RefuseManualStart --property=KillMode --property=SendSIGKILL 2>/dev/null) || return 1
   printf '%%s\n' "$vibepollo_new_host_properties" | grep -qx 'RefuseManualStart=no' && \
-    printf '%%s\n' "$vibepollo_new_host_properties" | grep -qx 'KillMode=control-group' && \
+    printf '%%s\n' "$vibepollo_new_host_properties" | grep -qx 'KillMode=mixed' && \
     printf '%%s\n' "$vibepollo_new_host_properties" | grep -qx 'SendSIGKILL=no' || return 1
   timeout --signal=KILL 15 systemctl unmask --runtime vibeshine-vkms-control.socket 2>/dev/null || return 1
   systemctl start vibeshine-vkms-control.socket || return 1
@@ -1207,6 +1218,8 @@ vibepollo_quiesce_machine_host() {
   vibepollo_unit_is_masked vibepollo-session-exec.socket || return 1
   vibepollo_stop_exact_unit vibepollo-session-exec.socket
   vibepollo_unit_is_quiescent vibepollo-session-exec.socket || return 1
+  vibepollo_stop_exact_unit vibepollo.service
+  vibepollo_unit_is_quiescent vibepollo.service || return 1
   vibepollo_stop_brokers || return 1
   for vibepollo_unit in vibepollo-session-controller.service vibepollo.service \
     vibepollo-prelogin.service vibepollo-machine-prepare.service; do
@@ -1223,6 +1236,8 @@ vibepollo_quiesce_machine_host() {
   vibepollo_unit_is_masked vibepollo.service || return 1
   vibepollo_unit_is_masked vibepollo-session-exec.socket || return 1
   vibepollo_stop_exact_unit vibepollo-session-exec.socket
+  vibepollo_stop_exact_unit vibepollo.service
+  vibepollo_unit_is_quiescent vibepollo.service || return 1
   vibepollo_stop_brokers || return 1
   for vibepollo_unit in vibepollo-session-exec.socket vibepollo-session-controller.service \
     vibepollo.service vibepollo-prelogin.service vibepollo-machine-prepare.service; do
@@ -1556,6 +1571,8 @@ vibepollo_preun_quiesce() {
     vibepollo_preun_unit_is_masked vibepollo-session-exec.socket || return 1
     vibepollo_preun_stop_exact_unit vibepollo-session-exec.socket
     vibepollo_preun_unit_is_quiescent vibepollo-session-exec.socket || return 1
+    vibepollo_preun_stop_exact_unit vibepollo.service
+    vibepollo_preun_unit_is_quiescent vibepollo.service || return 1
     vibepollo_preun_stop_brokers || return 1
     for vibepollo_unit in vibepollo-session-controller.service vibepollo.service \
       vibepollo-prelogin.service vibepollo-machine-prepare.service; do
@@ -1576,6 +1593,8 @@ vibepollo_preun_quiesce() {
     vibepollo_preun_unit_is_masked vibepollo.service || return 1
     vibepollo_preun_unit_is_masked vibepollo-session-exec.socket || return 1
     vibepollo_preun_stop_exact_unit vibepollo-session-exec.socket
+    vibepollo_preun_stop_exact_unit vibepollo.service
+    vibepollo_preun_unit_is_quiescent vibepollo.service || return 1
     vibepollo_preun_stop_brokers || return 1
     for vibepollo_unit in vibepollo-session-exec.socket vibepollo-session-controller.service \
       vibepollo.service vibepollo-prelogin.service vibepollo-machine-prepare.service; do

tests/unit/platform/linux/test_machine_service_contract.py
@@ -195,7 +195,7 @@ require(
 )
 for forbidden in ("ExecStart=", "ExecStartPre=", "WantedBy="):
     forbid(kwin_environment_dropin, forbidden, "KWin session environment drop-in")
-require(controller, 'stop_host "$deadline" || success=1', "fail-closed transition ordering")
+require(controller, 'stop_host "$deadline" || return 1', "fail-closed transition ordering")
 require(controller, "binding_matches_candidate complete", "display credential rebinding")
 require(controller, "observe_active_session || return 1", "authoritative transition snapshot")
 require(controller, "read_user_environment || return 1", "current transition credentials")
@@ -226,7 +226,7 @@ quiesce_body = controller.split("\nquiesce() {\n", 1)[1].split("\n}\n\nnext_gene
 quiesce_normalized = " ".join(quiesce_body.split())
 quiesce_order = (
     quiesce_normalized.index('close_broker_admission "$deadline" || return 1'),
-    quiesce_normalized.index('stop_host "$deadline" || success=1'),
+    quiesce_normalized.index('stop_host "$deadline" || return 1'),
     quiesce_normalized.index('stop_broker_instances "$deadline" || success=1'),
     quiesce_normalized.index('stop_bound_session_apps "$deadline" || success=1'),
     quiesce_normalized.rindex('stop_broker_instances "$deadline" || success=1'),
@@ -300,7 +300,7 @@ for unit_text, label in (
     forbid(unit_text, "PartOf=vibepollo-session-controller.service", f"{label} ordered controller cleanup")
 require(broker_unit.split("[Service]", 1)[0], "CollectMode=inactive-or-failed",
         "completed rejected broker requests must not exhaust handoff inventory")
-require(host_unit, "KillMode=control-group", "machine host process-tree shutdown")
+require(host_unit, "KillMode=mixed", "supervisor-owned host shutdown")
 require(host_unit, "SendSIGKILL=no", "GPU-owner graceful shutdown")
 require(host_unit, "Type=notify", "encoder-gated machine host readiness")
 require(host_unit, "NotifyAccess=all", "encoder-gated machine host readiness")
@@ -317,13 +317,12 @@ require(host_unit, "ProtectHome=yes", "machine host unit")
 require(host_unit, "PrivateTmp=yes", "machine host unit")
 require(host_unit, "RestrictSUIDSGID=yes", "machine host unit")
 for forbidden in (
-    "User=root", "CAP_DAC_OVERRIDE", "machine-prepare", "ExecStopPost=", "WantedBy=", "KillMode=mixed",
+    "User=root", "CAP_DAC_OVERRIDE", "machine-prepare", "ExecStopPost=", "WantedBy=", "KillMode=control-group",
     "BindsTo=vibepollo-session-controller.service",
 ):
     forbid(host_unit_directives, forbidden, "machine host unit")
-require(host, "trap mark_host_shutdown TERM INT HUP", "single service signal delivery")
+require(host, "trap request_host_shutdown TERM INT HUP", "single service signal delivery")
 forbid(host, "trap 'forward_host_signal", "duplicate service signal delivery")
-require(host, "((shutting_down)) || terminate_host", "single child termination request")
 
 for variable in ("VIBEPOLLO_MACHINE_HOST", "VIBEPOLLO_SESSION_ROLE"):
     require(host, variable, "machine-host provider scan environment")
@@ -789,6 +788,8 @@ for lifecycle, label in (
         "RefuseManualStart=yes",
         "vibepollo_upgrade_kill_mode=process",
         "vibepollo_upgrade_kill_mode=control-group",
+        "vibepollo_upgrade_kill_mode=mixed",
+        "trap request_host_shutdown TERM INT HUP",
         "SendSIGKILL=no",
         "--property=Job",
         "systemctl freeze",
@@ -939,7 +940,7 @@ for lifecycle, label in (
         "vibepollo_unmask_host_for_controller",
         "90-vibepollo-safe-upgrade.conf",
         "RefuseManualStart=no",
-        "KillMode=control-group",
+        "KillMode=mixed",
         "SendSIGKILL=no",
         "unmask --runtime vibeshine-vkms-control.socket",
         "start vibeshine-vkms-control.socket",